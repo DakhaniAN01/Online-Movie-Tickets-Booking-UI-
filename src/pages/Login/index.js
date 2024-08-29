3d3e7bcf8c29870bd18817d42b0d698e33818d7d
DakhaniAN01 <dakhani.an01@gmail.com>
2024-08-29 14:17:08 +0530

    UI Set Up

---
+++ src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, Flex } from 'antd';
+
+function Login() {
+  return (
+    <div>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  )
+}
+
+export default Login
