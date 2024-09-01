b5c5f4524920804f4fb2db27d1a49da560c1492e
DakhaniAN01 <dakhani.an01@gmail.com>
2024-09-02 01:00:49 +0530

    fonts and background color added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import {BrowserRouter ,Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import "./stylesheet/alignment.css";
+import "./stylesheet/custom.css";
+import "./stylesheet/form-elements.css";
+import "./stylesheet/sizes.css";
+import "./stylesheet/theme.css";
 
 function App() {
   return (
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { Button, Form } from 'antd';
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='flex justify-center h-screen items-center bg-primary'>
+      <div className="card p-3">
+        <h1 className='text-xl'>
+          BOOKMYTICKETS - REGISTER
+          <Form
+            layout='vertical'
+          
+          >
+            <Form.Item
+              label="Name"
+              name="name"
+              rule={[{ required: true, message: "Please enter your name!" }]}
+            >
+              <input type="text" />
+            </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              rule={[{ required: true, message: "Please enter your email!" }]}
+            >
+              <input type="email" />
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rule={[{ required: true, message: "Please enter your password!" }]}
+            >
+              <input type="password" />
+            </Form.Item>
+
+
+          </Form>
+
+        </h1>
+      </div>
+
+    </div>
   )
 }
 
